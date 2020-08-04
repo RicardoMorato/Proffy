044f0e1d5983862d22d28483d8fe9802152fafa6
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-04 12:14:14 -0300

    main(routes&&teacher-list): Added routes to project and created page TeacherList.

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 import logoImg from '../../assets/images/logo.svg';
@@ -20,15 +21,15 @@ export default function Landing() {
         <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
 
         <div className="buttons-container">
-          <a href="" className='study'>
+          <Link to='/estudar' className='study'>
             <img src={studyIcon} alt="" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className='give-classes'>
+          <Link to='/dar-aulas' className='give-classes'>
             <img src={giveClassesIcon} alt="" />
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function TeacherForm() {
+  return (
+    <p>Dale boy!</p>
+  );
+}
