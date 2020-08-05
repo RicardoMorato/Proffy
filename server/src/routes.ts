d6b85fc379afa4914d24107f78f8886282c3bd8a
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-05 11:45:11 -0300

    main(server): Added index controller to Classes.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,15 +1,9 @@
-import express, { response } from "express";
+import express from "express";
+import ClassesController from "./controllers/ClassesController";
 
-import db from "./database/connection";
-import convertHourToMinutes from "./utils/convertHourToMinutes";
 
 const routes = express.Router();
-
-interface scheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
+const classesController = new ClassesController();
 
 routes.get("/", (req, res) => {
   res.json({
@@ -19,53 +13,7 @@ routes.get("/", (req, res) => {
   });
 });
 
-routes.post("/classes", async (req, res) => {
-  const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body;
-
-  const trx = await db.transaction();
-
-  try {
-    const insertedUsersIds = await trx("users").insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    });
-
-    const user_id = insertedUsersIds[0];
-
-    const insertedClassesIds = await trx("classes").insert({
-      subject,
-      cost,
-      user_id,
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((item: scheduleItem) => {
-      return {
-        class_id,
-        week_day: item.week_day,
-        from: convertHourToMinutes(item.from),
-        to: convertHourToMinutes(item.to),
-      };
-    });
-
-    await trx("class_schedule").insert(classSchedule);
-
-    await trx.commit();
-
-    return res.status(201).json({
-      Message: "Class created",
-    });
-  } catch (err) {
-    await trx.rollback();
-
-    return res.status(400).json({
-      Message: 'Unexpected error while creating new class',
-      error: err,
-    });
-  }
-});
+routes.post("/classes", classesController.store);
+routes.get("/classes", classesController.index);
 
 export default routes;
