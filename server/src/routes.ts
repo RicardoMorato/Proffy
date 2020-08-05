c2e345cc5af20780347b60cd47b924707f8707f0
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-05 11:58:30 -0300

    main(server): Created connections controller.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,12 @@
 import express from "express";
+
 import ClassesController from "./controllers/ClassesController";
+import ConnectionsController from "./controllers/ConnectionsController";
 
 
 const routes = express.Router();
 const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 routes.get("/", (req, res) => {
   res.json({
@@ -15,5 +18,7 @@ routes.get("/", (req, res) => {
 
 routes.post("/classes", classesController.store);
 routes.get("/classes", classesController.index);
+
+routes.post('/connections', connectionsController.create);
 
 export default routes;
