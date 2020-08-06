ab2057fc08d7cfe24c9448ff4c9d7e6eefa8431c
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-06 13:59:55 -0300

    main(TeacherForm): Added Footer to section.

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,35 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import PageHeader from '../../components/PageHeader';
-import './styles.css';
+import PageHeader from "../../components/PageHeader";
+import "./styles.css";
+import AppInput from "../../components/AppInput";
+
+import warningIcon from "../../assets/images/icons/warning.svg";
 
 export default function TeacherForm() {
   return (
-    <div id="page-teacher-form" className='container'>
+    <div id="page-teacher-form" className="container">
       <PageHeader
-        title='Que incrível que você quer dar aulas.'
-        description='O primeiro passo é preencher esse formulário de inscrição'
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de inscrição"
       />
 
       <main>
         <fieldset>
           <legend>Seus dados</legend>
 
-          <div className="input-block">
-            <label htmlFor="name">Nome completo</label>
-            <input type="text" name="name" id="name" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="avatar">Avatar</label>
-            <input type="text" name="avatar" id="avatar" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="whatsapp">Whatsapp</label>
-            <input type="text" name="whatsapp" id="whatsapp" />
-          </div>
+          <AppInput name="name" label="Nome completo" />
+          <AppInput name="avatar" label="Avatar" />
+          <AppInput name="whatsapp" label="Whatsapp" />
         </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <AppInput name="subject" label="Matéria" />
+          <AppInput name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br/>
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
       </main>
     </div>
   );
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,28 +1,18 @@
 import React from "react";
 
-import PageHeader from "../../components/PageHeader";
 import "./styles.css";
+import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
+import AppInput from "../../components/AppInput";
 
 export default function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" name="subject" id="subject" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" name="week-day" id="week-day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" name="time" id="time" />
-          </div>
+          <AppInput name="subject" label="Matéria" />
+          <AppInput name="week-day" label="Dia da semana" />
+          <AppInput name="time" label="Hora" type="time" />
         </form>
       </PageHeader>
 
