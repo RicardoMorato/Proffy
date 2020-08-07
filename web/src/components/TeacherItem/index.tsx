ea9b1cce355215ff15b1e71338499001a7175d86
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-06 23:33:54 -0300

    main(TeacherItem): Made connections backend integration.

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
 export interface Teacher {
   avatar: string;
@@ -14,26 +15,27 @@ export interface Teacher {
   whatsapp: string;
 }
 interface TeacherItemProps {
-  teacher: Teacher
+  teacher: Teacher;
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("/connections", {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src={teacher.avatar}
-          alt="Foto de perfil"
-        />
+        <img src={teacher.avatar} alt="Foto de perfil" />
         <div>
           <strong>{teacher.name}</strong>
           <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        {teacher.bio}
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
@@ -41,13 +43,18 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           <strong>{teacher.cost}</strong>
         </p>
 
-        <button type="button">
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
