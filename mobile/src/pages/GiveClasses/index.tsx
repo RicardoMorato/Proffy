16dbea6b1ac9e0076356a6a58d01460f603c35fe
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-09 15:33:34 -0300

    main(mobile): Added folder to project again

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, ImageBackground, Text } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+
+import styles from "./styles";
+
+import giveClassesBgImages from "../../assets/images/give-classes-background.png";
+
+export default function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={giveClassesBgImages}
+        style={styles.content}
+        resizeMode="contain"
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar na nossa plataforma web
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
+    </View>
+  );
+}
