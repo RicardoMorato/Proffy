2e90c543f47c21bd4c1f33feb295a7b46afb431c
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-09 21:15:41 -0300

    main(mobile): Added ScrollView to pages.

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View, ScrollView } from "react-native";
 
-import styles from './styles';
-import PageHeader from '../../components/PageHeader';
+import styles from "./styles";
+import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
 export default function Favorites() {
   return (
     <View style={styles.container}>
-      <PageHeader title='Meus proffys favoritos' />
+      <PageHeader title="Meus proffys favoritos" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
