fe12fff79dacb6eeacb5fc31495fd50da9acb020
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-08-09 21:09:20 -0300

    main(mobile): Created component TeacherItem.

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -3,11 +3,14 @@ import { View } from "react-native";
 
 import styles from "./styles";
 import PageHeader from "../../components/PageHeader";
+import TeacherItem from "../../components/TeacherItem";
 
 export default function TeacherList() {
   return (
     <View style={styles.container}>
       <PageHeader title="Proffys disponíveis" />
+
+      <TeacherItem />
     </View>
   );
 }
